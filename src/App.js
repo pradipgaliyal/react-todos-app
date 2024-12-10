43c1afd5cada89975df209019f5bc43fa8b3ac6f
Pradip Galiyal <pradipgaliyal21@gmail.com>
2024-12-10 23:54:44 +0530

    first_day_code

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">Your Todo's</div>
+        <div className="card-body">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Description</th>
+                <th scope="col">Assign</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Feed Dog</td>
+                <td>Eric</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Get haircut</td>
+                <td>Eric</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
